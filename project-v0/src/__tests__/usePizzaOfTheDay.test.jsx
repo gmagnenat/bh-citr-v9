1019a6421d9bcb9a2d96c3ae41ed337ce0c2b44b
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-21 14:38:24 +0100

    test: add test for the contact route and test for the custom hook pizzaOfTheDay

---
+++ project-v0/src/__tests__/usePizzaOfTheDay.test.jsx
@@ -0,0 +1,22 @@
+import { expect, test, vi } from "vitest";
+import { renderHook } from "@testing-library/react";
+import createFetchMock from "vitest-fetch-mock";
+import { usePizzaOfTheDay } from "../usePizzaOfTheDay";
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+const testPizza = {
+  id: "calabrese",
+  name: "The Calabrese Pizza",
+  category: "Supreme",
+  description: "Spicy salami, mozzarella, and olives",
+  image: "/public/pizzas/calabrese.webp",
+  size: { S: 12.25, M: 16.25, L: 20.25 },
+};
+
+test("gives null when first called", async () => {
+  fetch.mockResponseOnce(JSON.stringify(testPizza));
+  const { result } = renderHook(() => usePizzaOfTheDay());
+  expect(result.current).toBeNull();
+});
